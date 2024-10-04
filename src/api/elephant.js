ea630dd125b7a18fb181fb35875435a6c8a0ce49
Glitch (glitch-hello-react) <none>
2024-10-04 15:13:49 +0000

    👓🌒 Checkpoint ./src/api/elephant.js:48120569/120

--- src/api/elephant.js
+++ src/api/elephant.js
@@ -706,8 +706,8 @@ const options = {
       `https://api.dexscreener.com/latest/dex/tokens/${SVNN},${GNME}`
     );
     console.log(response.data)
-    const priceA = response.data.pairs.find(pair => pair.baseToken.address===SVNN).priceUsd;
-    const priceB = response.data.pairs.find(pair => pair.baseToken.address===GNME).priceUsd;    
+    const priceA = response.data.pairs.find(pair => pair.baseToken.address === "DbM7mcJM9zitHanzKmFf7NH4SaEZZDCf5TPEgzwTmuh4").priceUsd;
+    const priceB = response.data.pairs.find(pair => pair.baseToken.address === "BaDjVCpABEVCdt4LT7ivuzA4izBwJCqnDjrLa8XBtT38").priceUsd;    
     return [priceA, priceB];
   } catch (err) {
     console.log(err);
