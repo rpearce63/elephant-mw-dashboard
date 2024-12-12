c9b65930a1c43132d16e27a3abbc53433f9cbc43
Glitch (glitch-hello-react) <none>
2024-12-12 17:25:26 +0000

    📍🍽 Checkpoint ./src/components/NFTCollection.jsx:48120569/3643

--- src/components/NFTCollection.jsx
+++ src/components/NFTCollection.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "wouter";
 import {getNFTsOfOwner} from "../api/elephant";
+import Button from "@mui/material/Button";
 
 export default () => {
   
-  const [nfts, setNfts] = useState([])
+  const [nfts, setNfts] = useState([]);
+  const [filtered, setFiltered] = useState([])
   const {account, count} = useParams();
   const [loading, setLoading] = useState(false);
+  const [source, setSource] = useState("all");
+  
   
   useEffect(() => {
   
@@ -15,6 +19,7 @@ export default () => {
       const nfts = await getNFTsOfOwner(address);
       
       setNfts(nfts.sort((a,b) => b.traits.score - a.traits.score));
+      setFiltered(nfts);
       setLoading(false)
     }
     getNfts(account);
@@ -26,11 +31,28 @@ export default () => {
   },[account]);
   
   
+  useEffect(() => {
+    source === "marketplace" && setFiltered(nfts.filter(nft => nft.price > 0));
+    source === "all" && setFiltered(nfts);
+  }, [source])
+  
   
  return <div className="nfts">
-   <h3 className="page-title" style={{width: "100%"}}>Total NFTs: {nfts.length || count}</h3>
+   <h3 className="page-title" style={{width: "100%"}}>Total NFTs: {nfts.length || count}
+     
+     <Button variant={source === "marketplace" ? "contained" : "outlined"}
+            size="small"
+            sx={{marginRight: "1em", marginLeft: "1em"}} 
+       onClick={e => setSource("marketplace")}>Marketplace</Button>
+     <Button 
+       variant={source === "all" ? "contained" : "outlined"}
+            size="small"
+            sx={{marginRight: "1em"}} 
+       onClick={e => setSource("all")}>All</Button>
+   </h3>
    
-   {loading ? <h3 className="page-title">...Loading NFTs</h3> : nfts.map((nft, index) => 
+  
+   {loading ? <h3 className="page-title">...Loading NFTs</h3> : filtered.map((nft, index) => 
              <div key={index} className="nft">
                <a href={`https://bnb.nftscan.com/0xb92afeDC8f8618BE4198fbE5d97adB7C60aB3198/${nft.tokenId}`} target="_blank" rel="noreferrer">
              <img  src={nft.image} width="200" /></a>
